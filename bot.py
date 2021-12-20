624a78cfdf20fa1fdf58ffe6179ff908d9f9b0d5
Ley Low <chuenleylow@gmail.com>
2021-12-21 04:17:12 +1100

    produces top 15 most popular words, need to add in additional preprocessing

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from dotenv import load_dotenv
+import os 
+import json
+load_dotenv()
+
+token = os.environ.get("bot-token")
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$scrapehistory'):
+        print("Scraping history of chat logs...")
+        history = await scrape_history(message)
+
+        with open("message_logs.json", 'w') as outfile:
+            json.dump(history, outfile)
+
+        await message.delete()
+        #await message.channel.send(history)
+
+async def scrape_history(message):
+    author_dict = {}
+    history = await message.channel.history(limit = 5000).flatten()
+    for message in history:
+        if (message.author.bot):
+            print(message.author)
+            continue
+
+        if (message.author.name in author_dict):
+            author_dict[message.author.name].append(message.content)
+        else:
+            author_dict[message.author.name] = [message.content]
+        #print(f"{message.author}: {message.content}")
+
+    #print(author_dict)
+    return author_dict
+
+client.run(token)
